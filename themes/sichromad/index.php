c3de33afabe7408b4353af24dffc1a247bcaedd5
azizulsoftsw <azizul.softsw@gmail.com>
2020-02-20 23:17:13 +0600

    Update index.php

---
+++ themes/sichromad/index.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+<section class="sc-blog-content-sec-wrp">
+  <div class="container sc-block-bg">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="sc-blog-dsc-wrp">
+          <h1>Where does it come from?</h1>
+          <div class="sc-blog-dsc-innr clearfix">
+            <div class="sc-blog-dsc-img">
+              <img src="<?php echo THEME_URI; ?>/assets/images/blog-post-img-1.png">
+            </div>
+            <div class="sc-blog-dsc">
+              <h2>What is Lorem Ipsum?</h2>
+              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            </div>
+          </div>
+          <div class="sc-blog-dsc-innr clearfix">
+            <div class="sc-blog-dsc-img">
+              <img src="<?php echo THEME_URI; ?>/assets/images/blog-post-img-1.png">
+            </div>
+            <div class="sc-blog-dsc">
+              <h2>What is Lorem Ipsum?</h2>
+              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            </div>
+          </div>
+          <div class="sc-blog-dsc-innr clearfix">
+            <div class="sc-blog-dsc-img">
+              <img src="<?php echo THEME_URI; ?>/assets/images/blog-post-img-1.png">
+            </div>
+            <div class="sc-blog-dsc">
+              <h2>What is Lorem Ipsum?</h2>
+              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            </div>
+          </div>
+        </div>
+        <div class="sc-blog-pagination text-center">
+          <ul class="clearfix">
+            <li><a href="#"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a></li>
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
